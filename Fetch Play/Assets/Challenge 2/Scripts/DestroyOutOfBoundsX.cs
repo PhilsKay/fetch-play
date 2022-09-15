9cf1d9cfb5b1c00e4bfaebaf903a3dbfc6be4683
PhilsKay <philipinhokayode@gmail.com>
2022-09-15 13:47:01 +0100

    add

---
+++ Fetch Play/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float bottomLimit = 0;
+    private float leftLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+            Debug.Log("Game Over");
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.z < leftLimit)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Fetch Play/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private Vector3 addPosition = new  Vector3(0,0,-2);
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab,transform.position + addPosition, dogPrefab.transform.rotation);
+        }
+    }
+}
